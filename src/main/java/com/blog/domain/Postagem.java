8ecd019d8508ddcbf03b6aee59788a0483404f67
Rafael <rafaelsouzavalle@gmail.com>
2021-07-24 15:51:42 -0300

    Altere as classes de domínio

--- src/main/java/com/blog/domain/Postagem.java
+++ src/main/java/com/blog/domain/Postagem.java
@@ -2,30 +2,111 @@ package com.blog.domain;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.sun.istack.NotNull;
 
 
 @Entity
 public class Postagem {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@GeneratedValue (strategy = GenerationType.SEQUENCE)
 	private Long id;
 	private String titulo;
 	private String conteudo;
-	private List<String> tags;
+	@OneToMany
+	private List<Tag> tags;
+	@DateTimeFormat(pattern = "dd-MM-yyyy hh:mm:ss")
 	private LocalDateTime dataHora;
 	
 	public Postagem() {
-		
+		atualizaHora();
+	}
+
+	public Postagem(String titulo, String conteudo, List<Tag> tags) {		
+		this.titulo = titulo;
+		this.conteudo = conteudo;
+		this.tags = tags;
+		atualizaHora();
 	}
 	
-	
-	
+	private void atualizaHora() {
+//		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy hh:mm:ss");
+//		String agora = dtf.format();
+//		LocalDateTime date = LocalDateTime.parse(agora, dtf);
+		this.dataHora = LocalDateTime.now();
+	}
 
-   
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long i) {
+		this.id = i;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getConteudo() {
+		return conteudo;
+	}
+
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+
+	public List<Tag> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<Tag> tags) {
+		this.tags = tags;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Postagem other = (Postagem) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Postagem [id=" + id + ", titulo=" + titulo + ", conteudo=" + conteudo + ", tags=" + tags + ", dataHora="
+				+ dataHora + "]";
+	}
 }
---
+++ src/main/java/com/blog/domain/Tag.java
@@ -0,0 +1,74 @@
+package com.blog.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Tag {
+
+	@Id
+	@NotNull
+	@GeneratedValue (strategy = GenerationType.SEQUENCE)
+	private Long id;
+	private String texto;
+	
+	public Tag() {
+	}
+	
+	public Tag(String texto) {
+		this.texto = texto;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Tag [id=" + id + ", texto=" + texto + "]";
+	}
+	
+	
+	
+}
