c5974d0130bf29d187d24f3b6df8046fa75e160c
Rafael <rafaelsouzavalle@gmail.com>
2021-07-24 22:55:52 -0300

    Autorize no controller o acesso do backend para a API Rest
    Autorize no controller o acesso do backend para a API Rest, possibilitando que haja requisições da porta 4200 para a 8080.

--- src/main/java/com/blog/controller/PostagemController.java
+++ src/main/java/com/blog/controller/PostagemController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.domain.Postagem;
@@ -18,6 +20,8 @@ import com.blog.domain.Tag;
 import com.blog.repository.PostagemRepository;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200/")
+@RequestMapping("/")
 public class PostagemController {
 
 	@Autowired
